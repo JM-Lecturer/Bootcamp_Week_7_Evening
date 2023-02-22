276348f60185a311b15d31cd59f2eb38ff66c1d2
Jamie Miles <jamie.miles@weston.ac.uk>
2023-02-22 19:41:59 +0000

    Added change to display which CAN now include the stock

--- Bootcamp_Week_7_Evening/Bootcamp_Week_7_Evening.py
+++ Bootcamp_Week_7_Evening/Bootcamp_Week_7_Evening.py
@@ -1,14 +1,19 @@
+from pickle import TRUE
+
+
 class Vehicle:
     colour = ""
     wheelsCount = 0
     maxSpeed = 0
     name = ""
+    stock = 0
 
-    def __init__(self, colour, wheelCount, maxSpeed, name):
+    def __init__(self, colour, wheelCount, maxSpeed, name, stock):
         self.colour = colour
         self.wheelsCount = wheelCount
         self.maxSpeed = maxSpeed
         self.name = name
+        self.stock = stock
 
 def RunMenu(MenuItems):
     print(MenuItems[0])
@@ -19,12 +24,17 @@ def RunMenu(MenuItems):
     choice = int(input("\nPlease select from the options above\n"))
     return choice
 
-def DisplayVehicles(Vehicles):
+def DisplayVehicles(Vehicles, stock):
     print("----------Vehicles----------\n")
 
     for i in range(0, len(Vehicles)):
-        print("Vehicle " + str(i + 1) + "\n Name: " + Vehicles[i].name + "\n Colour: " + Vehicles[i].colour + "\n Wheels: " + str(Vehicles[i].wheelsCount) + "\n Max Speed: " + str(Vehicles[i].maxSpeed) + "\n")
+        print("Vehicle " + str(i + 1) + "\n Name: " + Vehicles[i].name + "\n Colour: " + Vehicles[i].colour + "\n Wheels: " + str(Vehicles[i].wheelsCount) + "\n Max Speed: " + str(Vehicles[i].maxSpeed))
+        if stock == True:
+            print(" Stock: " + str(Vehicles[i].stock))
+        print("")
 
+def PurchaseVehicles(Vehicles):
+    DisplayVehicles(Vehicles, True)
 
 MenuItems = list()
 MenuItems.append("----------Menu----------\n")
@@ -33,11 +43,11 @@ MenuItems.append("Purchase Items")
 MenuItems.append("Exit")
 
 Vehicles = list()
-Vehicles.append(Vehicle("Red", 2, 10, "Bicycle"))
-Vehicles.append(Vehicle("Yellow", 0, 40, "Submarine"))
-Vehicles.append(Vehicle("Black", 4, 70, "Taxi"))
-Vehicles.append(Vehicle("Green", 16, 50, "Tank"))
-Vehicles.append(Vehicle("White", 3, 130, "Airplane"))
+Vehicles.append(Vehicle("Red", 2, 10, "Bicycle", 200))
+Vehicles.append(Vehicle("Yellow", 0, 40, "Submarine", 1))
+Vehicles.append(Vehicle("Black", 4, 70, "Taxi", 12))
+Vehicles.append(Vehicle("Green", 16, 50, "Tank", 2))
+Vehicles.append(Vehicle("White", 3, 130, "Airplane", 3))
 
 loop = True
 while loop == True:
@@ -45,9 +55,9 @@ while loop == True:
     choice = RunMenu(MenuItems)
 
     if choice == 1:
-        DisplayVehicles(Vehicles)
+        DisplayVehicles(Vehicles, False)
     elif choice == 2:
-        print("Purchasing coming soon...\n")
+        PurchaseVehicles(Vehicles)
     else:
         print("\nGoodbye")
         loop = False
