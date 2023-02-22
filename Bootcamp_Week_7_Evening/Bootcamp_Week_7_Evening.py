8a16366e94417db70e68956eddeb01c183980485
Jamie Miles <jamie.miles@weston.ac.uk>
2023-02-22 18:02:00 +0000

    Started Menu Functions, also added List of items

--- Bootcamp_Week_7_Evening/Bootcamp_Week_7_Evening.py
+++ Bootcamp_Week_7_Evening/Bootcamp_Week_7_Evening.py
@@ -1,4 +1,14 @@
-print("----------Menu----------\n\n")
+def RunMenu(MenuItems):
+    print(MenuItems[0])
+
+print("----------Static Menu----------\n\n")
 print("1: Start")
 print("2: Section 2")
 print("3: Exit")
+
+MenuItems = list()
+MenuItems.append("----------Dynamic Menu----------\n\n")
+MenuItems.append("View Stock")
+MenuItems.append("Purchase Items")
+MenuItems.append("Exit")
+RunMenu(MenuItems)
