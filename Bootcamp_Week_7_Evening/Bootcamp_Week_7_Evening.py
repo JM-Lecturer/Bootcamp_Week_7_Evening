87542a9e43f51495ce06ac28d828f8d004972817
Jamie Miles <jamie.miles@weston.ac.uk>
2023-02-22 18:52:15 +0000

    Created vehicle class

--- Bootcamp_Week_7_Evening/Bootcamp_Week_7_Evening.py
+++ Bootcamp_Week_7_Evening/Bootcamp_Week_7_Evening.py
@@ -1,3 +1,15 @@
+class Vehicles:
+    colour = ""
+    wheelsCount = 0
+    maxSpeed = 0
+    name = ""
+
+    def __init__(self):
+        self.colour = "Red"
+        self.wheelsCount = 2
+        self.maxSpeed = 10
+        self.name = "Bicycle"
+
 def RunMenu(MenuItems):
     print(MenuItems[0])
 
