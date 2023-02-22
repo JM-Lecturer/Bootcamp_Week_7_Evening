b0964bdb626f5aa1f45fe9e6fb5513c9d3050647
Jamie Miles <jamie.miles@weston.ac.uk>
2023-02-22 19:04:42 +0000

    Reformatted menu with loop + added if statement for their selection

--- Bootcamp_Week_7_Evening/Bootcamp_Week_7_Evening.py
+++ Bootcamp_Week_7_Evening/Bootcamp_Week_7_Evening.py
@@ -12,14 +12,29 @@ class Vehicles:
 
 def RunMenu(MenuItems):
     print(MenuItems[0])
-
+    #Add in validation for the users input!
     for i in range(1, len(MenuItems)):
         print(str(i) + ": " + MenuItems[i])
 
+    choice = int(input("\nPlease select from the options above\n"))
+    return choice
+
 
 MenuItems = list()
-MenuItems.append("----------Menu----------\n\n")
+MenuItems.append("----------Menu----------\n")
 MenuItems.append("View Stock")
 MenuItems.append("Purchase Items")
 MenuItems.append("Exit")
-RunMenu(MenuItems)
+
+loop = True
+while loop == True:
+
+    choice = RunMenu(MenuItems)
+
+    if choice == 1:
+        print("View coming soon...")
+    elif choice == 2:
+        print("Purchasing coming soon...\n")
+    else:
+        print("\nGoodbye")
+        loop = False
